3e702753b97f7e0e466d66db56d4e01a55a4416d
VINCENT NAAYEM <vincentnaayem@gmail.com>
2021-10-31 18:49:39 +0100

    added create_logger and Fakelogger in utils

--- code/deep_vitabuild/utils/utils.py
+++ code/deep_vitabuild/utils/utils.py
@@ -7,6 +7,51 @@ from easydict import EasyDict as edict
 from pathlib import Path
 import argparse
 
+def create_logger(output_dir, cfg):
+    if 'FAKERUN' in cfg and cfg.FAKERUN:
+        return FakeLogger()
+
+    exp_foldername = cfg.EXP_NAME
+    assert exp_foldername is not None, 'Specify the Experiment Name!'
+    assert output_dir is not None, 'Specify the Output folder (where to save models, checkpoints, etc.)!'
+
+    log_filename = f'{exp_foldername}'
+
+    i = 1
+    while True:
+        if (Path(output_dir) / Path(f'{log_filename}_{i:03d}.log')).exists():
+            i += 1
+        else:
+            log_filename = f'{log_filename}_{i:03d}'
+            break
+    
+    print(f'=> New log file "{log_filename}.log" is created.')
+
+    final_log_file = Path(output_dir) / Path(log_filename+'.log') 
+    logging.basicConfig(
+                        filename=str(final_log_file),# level=logging.INFO,
+                        format='%(asctime)-15s %(message)s', 
+                        datefmt='%d-%m-%Y, %H:%M:%S'
+                        )
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    console = logging.StreamHandler()
+    logging.getLogger('').addHandler(console)
+
+    return logger, log_filename
+
+class FakeLogger(object):
+
+    def __init__(self):
+        print()
+        print('#'*30)
+        print('#\n#   FAKE LOGGER is running... No proper saving of models/metrics/logging! \n#')
+        print('#'*30)
+        print()
+
+    def info(self, msg):
+        print(msg)
+
 def init_exp_folder(cfg):
     if 'FAKERUN' in cfg and cfg.FAKERUN:
         return None
