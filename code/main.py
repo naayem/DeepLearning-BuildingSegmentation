b2f3d6017e0d88c8a6c33a94fbb1556c2891f224
VINCENT NAAYEM <vincentnaayem@gmail.com>
2021-10-31 22:41:02 +0100

    Trainer removed from main

--- code/main.py
+++ code/main.py
@@ -1,38 +1,7 @@
 import pprint
 
-from torch._C import device
-from deep_vitabuild import core, utils, models
-from torch import device as torch_device
-
-class Trainer():
-    '''
-    Class, which runs the training procedure.
-    Also keep all necessary components together:
-            models, 
-            optimizers, 
-            schedulers, 
-            datasets,
-            dataloaders,
-            losses,
-            procedures.
-    This universal module is developed to run deep learning experiments of any kind.
-    '''
-    def __init__(self, cfg): # initialized by <configname>.yaml file
-        self.cfg = utils.config_utils.get_config(cfg) #Parses the cfg.yaml file in dict of dict fashion
-        self.final_output_dir = utils.utils.init_exp_folder(self.cfg)
-        self.logger, self.log_filename = utils.utils.create_logger(self.final_output_dir, self.cfg)
-
-        self.gpus = [int(i) for i in self.cfg.GPUS.split(',')]
-        self.device0 = torch_device(f'cuda:{self.gpus[0]}')
-        self.models = models.models_common.get_models_by_config(self.cfg, self.gpus, self.device0)
-    
-    def run(self):
-        '''Runs the training procedure specified in "self.proc". 
-        '''
-        for model in self.models:
-            model.run()
-        self.logger.info(f'\nTraining?? of "{self.cfg.EXP_NAME}" experiment is finished. \n')
-
+from deep_vitabuild import utils
+from deep_vitabuild import core
 
 def main():
 
