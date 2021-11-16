dc9e6208997404d0e42b0e32b654fb65155d1ffc
naayem <vincentnaayem@gmail.com>
2021-11-16 17:06:01 +0100

    clean_up

---
+++ code/TESTmain.py
@@ -0,0 +1,33 @@
+import pprint
+
+from deep_vitabuild import utils
+from deep_vitabuild import core
+
+def main():
+
+    ### initialize Trainer
+    # simply get the main.py arguments as namespace ( like a dict)
+    # The .cfg retrieve the corresponding path to the exp yaml
+    cfg = utils.utils.parse_args().cfg
+    print("ooooo", cfg)
+ 
+    trainer = core.trainer.Trainer(cfg)
+
+    ### copy yaml description file to the save folder
+    utils.utils.copy_exp_file(trainer)
+
+    ### copy proc.py file to the save folder
+    utils.utils.copy_proc_file(trainer)
+
+    trainer.logger.info(pprint.pformat(trainer.cfg))
+    trainer.logger.info('#'*100)
+
+    print(trainer.cfg.MODELS.model1.PARAMS.WEIGHT_PATH)
+
+    ### run the training procedure
+    #trainer.run()
+
+
+if __name__ == '__main__':
+
+    main()
